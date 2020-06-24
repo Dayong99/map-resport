a04c5534d52b5888600e040631cb240267276ecc
Dayong99 <2484030571@qq.com>
2020-06-24 17:38:48 +0800

    'next'

--- src/view/Weather.vue
+++ src/view/Weather.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <canvas class="canvas">{{parmas}}</canvas>
-    <div class="canvas">{{parmas}}</div>
+    <canvas id="canvas" width="770" height="290"></canvas>
+<!--    <div>{{parmas}}</div>-->
   </div>
 </template>
 
@@ -10,9 +10,11 @@
     data() {
       return {
         parmas: [],
-        lon: [],
-        lat: [],
+        py: Number,
+        px: Number,
         xMin: Number,
+        xMax: Number,
+        yMin: Number,
         yMax: Number,
       };
     },
@@ -25,21 +27,58 @@
           'convection/api/autostation/show?basic=' +
           '1&encrypt=1&time=2020-01-02%2014%3A50%3A00')
         this.parmas = res.data
-        for(let i in this.parmas){
-          this.lon = Object.values(this.parmas[i])[2]
-          console.log(this.lon);
-          this.lat = Object.values(this.parmas[i])[3]
+
+        let pyArr = []
+        let pxArr = []
+        for(let i =0;i<this.parmas.length;i++){
+          this.px = Object.values(this.parmas[i])[2]
+          pxArr.push(this.px)
+          this.py = Object.values(this.parmas[i])[3]
+          pyArr.push(this.py)
         }
-        // this.xMin = Math.min.apply(Math,this.lat)
-        // this.yMax = Math.max.apply(Math,this.lon)
+
+        this.xMax= Math.max.apply(Math,pxArr)
+        this.xMin= Math.min.apply(Math,pxArr)
+        this.yMax = Math.max.apply(Math,pyArr)
+        this.yMin = Math.min.apply(Math,pyArr)
+        console.log(this.xMax);
+        console.log(this.xMin);
+        console.log(this.yMax);
+        console.log(this.yMin);
+
+        const canvas = document.getElementById('canvas');
+        const ctx = canvas.getContext('2d');
+        ctx.scale(2,2);
+        ctx.fillStyle = 'red';
+
+        for(let i =0;i<this.parmas.length;i++) {
+          // ctx.fillRect(pxArr[i],pyArr[1],1,1)
+          ctx.fillRect(770,290,1,1)
+        }
+        // 点坐标
+        // let coordinate = {
+        //   x:this.xMin,
+        //   y:this.yMin
+        // }
+
+        // 点尺寸
+        // let dottedSize = 6
+        //
+        // ctx.moveTo(coordinate.x - dottedSize/2, coordinate.y - coordinate/2)
+        // ctx.moveTo(coordinate.x + dottedSize/2, coordinate.y - coordinate/2)
+        // ctx.moveTo(coordinate.x + dottedSize/2, coordinate.y + coordinate/2)
+        // ctx.moveTo(coordinate.x - dottedSize/2, coordinate.y + coordinate/2)
+        // ctx.closePath()
+        // ctx.fill()
+
       }
     }
   };
 </script>
 
 <style scoped>
-  .canvas {
-    background-color: #f0f3ff;
+  #canvas{
+    border:1px solid darkolivegreen;
   }
 
 </style>
