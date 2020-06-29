4c2c32deb8f901835278a628104a5af6d720aa22
Dayong99 <2484030571@qq.com>
2020-06-29 10:23:18 +0800

    'ok'

--- src/view/Weather.vue
+++ src/view/Weather.vue
@@ -76,23 +76,22 @@
         const fifthParmsX = 17 * (Math.sin((360 / 5 * 4) * Math.PI / 180)).toFixed(1)
         const fifthParmsY = 17 * (Math.cos((360 / 5 * 4) * Math.PI / 180)).toFixed(1)
 
-        // 绘制地点、参数
+        // 向canvas映射地点、参数
         for (let i = 0; i < this.parmas.length; i++) {
           // 地点
-          ctx.fillRect((pxArr[i] - this.xMin) * scaleX + 40, (this.yMax - pyArr[i]) * scaleY + 40, 4, 4)
+          ctx.fillRect((pxArr[i] - this.xMin) * scaleX + canvas.width*0.15, (this.yMax - pyArr[i]) * scaleY + canvas.height*0.15, 4, 4)
           // 参数
-          ctx.fillText(Object.values(this.parmas[i])[2], (pxArr[i] - this.xMin) * scaleX + 40 + firstParmsX - 15,
-            (this.yMax - pyArr[i]) * scaleY + 40 + firstParmsY + 8)
-          ctx.fillText(Object.values(this.parmas[i])[3], (pxArr[i] - this.xMin) * scaleX + 40 + secondParmsX + 3,
-            (this.yMax - pyArr[i]) * scaleY + 40 + secondParmsY + 2)
-          ctx.fillText(Object.values(this.parmas[i])[4], (pxArr[i] - this.xMin) * scaleX + 40 + thirdParmsX,
-            (this.yMax - pyArr[i]) * scaleY + 40 + thirdParmsY)
-          ctx.fillText(Object.values(this.parmas[i])[5], (pxArr[i] - this.xMin) * scaleX + 40 + fourthParmsX - 4,
-            (this.yMax - pyArr[i]) * scaleY + 40 + fourthParmsY)
-          ctx.fillText(Object.values(this.parmas[i])[6], (pxArr[i] - this.xMin) * scaleX + 40 + fifthParmsX - 20,
-            (this.yMax - pyArr[i]) * scaleY + 40 + fifthParmsY + 2)
+          ctx.fillText(Object.values(this.parmas[i])[2], (pxArr[i] - this.xMin) * scaleX + canvas.width*0.15 + firstParmsX - 15,
+            (this.yMax - pyArr[i]) * scaleY + canvas.height*0.15 + firstParmsY + 8)
+          ctx.fillText(Object.values(this.parmas[i])[3], (pxArr[i] - this.xMin) * scaleX + canvas.width*0.15 + secondParmsX + 3,
+            (this.yMax - pyArr[i]) * scaleY + canvas.height*0.15 + secondParmsY + 2)
+          ctx.fillText(Object.values(this.parmas[i])[4], (pxArr[i] - this.xMin) * scaleX + canvas.width*0.15 + thirdParmsX,
+            (this.yMax - pyArr[i]) * scaleY + canvas.height*0.15 + thirdParmsY)
+          ctx.fillText(Object.values(this.parmas[i])[5], (pxArr[i] - this.xMin) * scaleX + canvas.width*0.15 + fourthParmsX - 4,
+            (this.yMax - pyArr[i]) * scaleY + canvas.height*0.15 + fourthParmsY)
+          ctx.fillText(Object.values(this.parmas[i])[6], (pxArr[i] - this.xMin) * scaleX + canvas.width*0.15 + fifthParmsX - 20,
+            (this.yMax - pyArr[i]) * scaleY + canvas.height*0.15 + fifthParmsY + 2)
         }
-
         Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3YWQ2NGZkNC0yYjYzLTRiNzEtOWJjMi01Y2I5NGJlMzEyZGQiLCJpZCI6Mjk4NjAsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1OTI4MTI1NTR9.-zUaIBAgYOlJ-36v2q9uy2BK4xPendbHEbX8JjKav-s';
         let viewer = new Cesium.Viewer('cesiumContainer',{
           imageryProvider: Cesium.createWorldImagery({
@@ -114,8 +113,8 @@
 
         layers.addImageryProvider(
           new Cesium.SingleTileImageryProvider({
-            url:"../../assets/image.png",
-            rectangle: Cesium.Rectangle.fromDegrees(0, 0, 116.3, 39.9),
+            url:canvasUrl,
+            rectangle: Cesium.Rectangle.fromDegrees(110.056,30.525,132.077,33.43),
           })
         );
       }
