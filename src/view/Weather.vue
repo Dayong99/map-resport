04f8b9b28d65cf07b7e6f3f1ce20a8d0de764cdb
Dayong99 <2484030571@qq.com>
2020-07-02 18:53:29 +0800

    '7.2-18.53'

--- src/view/Weather.vue
+++ src/view/Weather.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
         <div id="cesiumContainer"></div>
-        <canvas id="canvas"></canvas>
+        <canvas id="canvas" :width = "getWidth" :height = "getHeight"></canvas>
     </div>
 </template>
 
 <script>
     import * as Cesium from "cesium";
-
     export default {
         data() {
             return {
@@ -20,17 +19,12 @@
                 yMax: Number,
                 canvasUrl: '',
                 scaleLevelL: '',
-
                 fontSize: 12,
-
-                width: 775,
-                height: 295,
-
+                // getWidth: 775,
+                // getHeight: 295,
                 scaleX: '',
                 scaleY: '',
-
                 radius: 17,
-
                 firstParmsX: Number,
                 firstParmsY: Number,
                 secondParmsX: Number,
@@ -41,52 +35,59 @@
                 fourthParmsY: Number,
                 fifthParmsX: Number,
                 fifthParmsY: Number,
-
                 img:''
             };
         },
         created() {
             this.get();
         },
-        watch: {
-            scaleLevelL(newVal) {
-                // console.log(this.scaleLevelL);
-                if(newVal === 2) {
-                    this.width = 775 * 2
-                    this.height = 295 * 2
-                }else if(newVal === 3) {
-                    this.width = 775 * 4
-                    this.height = 295 * 4
-                }else if(newVal === 4) {
-                    this.width = 775 * 6
-                    this.height = 295 * 6
-                }else if(newVal === 5) {
-                    this.width = 775 * 8
-                    this.height = 295 * 8
-                }else if(newVal === 6) {
-                    this.width = 775 * 10
-                    this.height = 295 * 10
-                }else if(newVal === 7) {
-                    this.width = 775 * 12
-                    this.height = 295 * 12
-                }else if(newVal === 8) {
-                    this.width = 775 * 14
-                    this.height = 295 * 14
-                }else if(newVal === 9) {
-                    this.width = 775 * 16
-                    this.height = 295 * 16
-                }else if(newVal === 10) {
-                    this.width = 775 * 18
-                    this.height = 295 * 18
-                }else if(newVal === 11) {
-                    this.width = 775 * 20
-                    this.height = 295 * 20
-                }else if(newVal === 12) {
-                    this.width = 775 * 22
-                    this.height = 295 * 22
-                }
+        computed: {
+            getWidth: function() {
+                return 775 * this.scaleLevelL * 1.8
+            },
+            getHeight: function() {
+                return 295 * this.scaleLevelL * 1.8
             }
         },
+        // watch: {
+        //     scaleLevelL(newVal) {
+        //         console.log(this.scaleLevelL);
+        //         if(newVal === 2) {
+        //             this.getWidth = 775 * 2
+        //             this.getHeight = 295 * 2
+        //         }else if(newVal === 3) {
+        //             this.getWidth = 775 * 4
+        //             this.getHeight = 295 * 4
+        //         }else if(newVal === 4) {
+        //             this.getWidth = 775 * 6
+        //             this.getHeight = 295 * 6
+        //         }else if(newVal === 5) {
+        //             this.getWidth = 775 * 8
+        //             this.getHeight = 295 * 8
+        //         }else if(newVal === 6) {
+        //             this.getWidth = 775 * 10
+        //             this.getHeight = 295 * 10
+        //         }else if(newVal === 7) {
+        //             this.getWidth = 775 * 12
+        //             this.getHeight = 295 * 12
+        //         }else if(newVal === 8) {
+        //             this.getWidth = 775 * 14
+        //             this.getHeight = 295 * 14
+        //         }else if(newVal === 9) {
+        //             this.getWidth = 775 * 16
+        //             this.getHeight = 295 * 16
+        //         }else if(newVal === 10) {
+        //             this.getWidth = 775 * 18
+        //             this.getHeight = 295 * 18
+        //         }else if(newVal === 11) {
+        //             this.getWidth = 775 * 20
+        //             this.getHeight = 295 * 20
+        //         }else if(newVal === 12) {
+        //             this.getWidth = 775 * 22
+        //             this.getHeight = 295 * 22
+        //         }
+        //     }
+        // },
         methods: {
             async get() {
                 const {data: res} = await this.axios('http://192.168.1.2:8301/'
@@ -103,10 +104,8 @@
                 const blackMarble = layers.addImageryProvider(
                     new Cesium.IonImageryProvider({assetId: 3812}),
                 );
-
                 blackMarble.alpha = 0.5;
                 blackMarble.brightness = 2.0;
-
                 layers.addImageryProvider(
                     new Cesium.SingleTileImageryProvider({
                         url: this.img,
@@ -124,10 +123,9 @@
                     let C = 91610.74
                     let D = -40467.74
                     this.scaleLevelL = Math.round(D + (A - D) / (1 + Math.pow(height/C, B)))
-                    console.log(this.scaleLevelL)
+                    // console.log(this.scaleLevelL)
                 })
             },
-
             // 获得canvas
             getCanvas() {
                 const pyArr = [];
@@ -138,27 +136,20 @@
                     this.py = Object.values(this.parmas[i])[3];
                     pyArr.push(this.py);
                 }
-
                 this.xMax = Math.max.apply(Math, pxArr);
                 this.xMin = Math.min.apply(Math, pxArr);
                 this.yMax = Math.max.apply(Math, pyArr);
                 this.yMin = Math.min.apply(Math, pyArr);
-
                 const canvas = document.getElementById('canvas');
                 const ctx = canvas.getContext('2d');
-
                 canvas.style.width = 775 + 'px'
                 canvas.style.width = 295 + 'px'
-
-                canvas.width = this.width
-                canvas.height = this.height
-
+                // canvas.width = getWidth
+                // canvas.height = getHeight
                 ctx.font = 'normal normal 500 12px serif';
-
                 // 放大倍数
                 this.scaleX = canvas.width / (this.xMax - this.xMin) * 0.7;
                 this.scaleY = canvas.height / (this.yMax - this.yMin) * 0.7;
-
                 // 参数位置
                 this.firstParmsX = this.radius * (Math.sin((0) * Math.PI / 180)).toFixed(1);
                 this.firstParmsY = this.radius * (Math.cos((0) * Math.PI / 180)).toFixed(1);
@@ -170,41 +161,35 @@
                 this.fourthParmsY = this.radius * (Math.cos((360 / 5 * 3) * Math.PI / 180)).toFixed(1);
                 this.fifthParmsX = this.radius * (Math.sin((360 / 5 * 4) * Math.PI / 180)).toFixed(1);
                 this.fifthParmsY = this.radius * (Math.cos((360 / 5 * 4) * Math.PI / 180)).toFixed(1);
-
                 // 向canvas映射地点、参数
                 for (let i = 0; i < this.parmas.length; i++) {
                     // 地点
                     ctx.fillStyle = '#f94b52';
                     ctx.fillRect((pxArr[i] - this.xMin) * this.scaleX + canvas.width * 0.15, (this.yMax - pyArr[i]) * this.scaleY + canvas.height * 0.15, 4, 4);
-
                     // 参数1
                     for (let i = 0; i < this.parmas.length; i++) {
                         ctx.fillStyle = '#a200ec';
                         ctx.fillText("" + Object.values(this.parmas[i])[2], (pxArr[i] - this.xMin) * this.scaleX + canvas.width * 0.15 + this.firstParmsX - 15,
                             (this.yMax - pyArr[i]) * this.scaleY + canvas.height * 0.15 + this.firstParmsY + 8,);
                     }
-
                     // 参数2
                     for (let i = 0; i < this.parmas.length; i++) {
                         ctx.fillStyle = '#289434';
                         ctx.fillText("" + Object.values(this.parmas[i])[3], (pxArr[i] - this.xMin) * this.scaleX + canvas.width * 0.15 + this.secondParmsX + 3,
                             (this.yMax - pyArr[i]) * this.scaleY + canvas.height * 0.15 + this.secondParmsY + 2);
                     }
-
                     // 参数3
                     for (let i = 0; i < this.parmas.length; i++) {
                         ctx.fillStyle = '#3a24ed';
                         ctx.fillText("" + Object.values(this.parmas[i])[4], (pxArr[i] - this.xMin) * this.scaleX + canvas.width * 0.15 + this.thirdParmsX,
                             (this.yMax - pyArr[i]) * this.scaleY + canvas.height * 0.15 + this.thirdParmsY);
                     }
-
                     // 参数4
                     for (let i = 0; i < this.parmas.length; i++) {
                         ctx.fillStyle = "#e01549"
                         ctx.fillText("" + Object.values(this.parmas[i])[5], (pxArr[i] - this.xMin) * this.scaleX + canvas.width * 0.15 + this.fourthParmsX - 4,
                             (this.yMax - pyArr[i]) * this.scaleY + canvas.height * 0.15 + this.fourthParmsY);
                     }
-
                     // 参数5
                     for (let i = 0; i < this.parmas.length; i++) {
                         ctx.fillStyle = "#1b5cf7"
@@ -214,7 +199,6 @@
                 }
                 return canvas
             },
-
             // 获取cesium
             getCesium() {
                 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3YWQ2NGZkNC0yYjYzLTRiNzEtOWJjMi01Y2I5NGJlMzEyZGQiLCJpZCI6Mjk4NjAsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1OTI4MTI1NTR9.-zUaIBAgYOlJ-36v2q9uy2BK4xPendbHEbX8JjKav-s';
@@ -234,5 +218,4 @@
     #canvas {
         border: 1px solid #0c6f58;
     }
-
 </style>
